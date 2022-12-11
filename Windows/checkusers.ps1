d1e8ce62e051674020843e54192f5c760aee8574
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2022-12-11 16:56:08 -0500

    Update checkusers.ps1

--- Windows/checkusers.ps1
+++ Windows/checkusers.ps1
@@ -1,2 +1,7 @@
-
-foreach ($
+$path=Split-Path .\users.txt
+$get= Get-content $path/path.txt
+$accounts = Get-Wmiobject Win32_UserAccount -filter 'LocalAccount=TRUE' | select-object -expandproperty Name
+Clear-content "$get\users.txt"
+foreach($l in $accounts){
+	$l >> "$get\users.txt"
+}
