dcb496478efe398971da373181414d3712bab2c2
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2022-12-13 13:50:04 -0500

    Update run.bat

--- Windows/run.bat
+++ Windows/run.bat
@@ -4,9 +4,21 @@
 REM GO TO SECPOL & USE SECEDIT
 pause
 
+:EnableFirewall
+NetSh Advfirewall set allprofiles state on
+
+
+:DisableRDP
+
+
 :Menu
 
 echo 1 = Set Local User Policy
+echo 2 = enable Firwall
+echo 3 = Disable RDP (Remote Desktop)
+echo 4 = Disable Guest & Administrator
+
+
 
 :ConfirmBeforeStart
 set /p i="Did you complete the forensic (y/n)"
