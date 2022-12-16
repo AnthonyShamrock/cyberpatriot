e5442a6449ef021b257aaa94f8b0862647127d39
AnthonyShamrock <72403570+AnthonyShamrock@users.noreply.github.com>
2022-12-16 09:10:18 -0500

    Update run.bat

--- Windows/run.bat
+++ Windows/run.bat
@@ -9,7 +9,13 @@ NetSh Advfirewall set allprofiles state on
 
 
 :DisableRDP
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+netsh advfirewall firewall set rule group="remote desktop" new enable=No
 
+
+:DisablePrinters
+net stop spoolers
+reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v LegacyDefaultPrinterMode /t REG_DWORD /d 1 /f
 
 :Menu
 
